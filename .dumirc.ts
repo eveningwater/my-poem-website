ee426f0644a0949cfc59455adec9b9c36d798ea0
eveningwater <854806732@qq.com>
2023-10-07 21:20:00 +0800

    feat: 修改了打包配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/my-poem-website/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-poem-website/' : '/',
   themeConfig: {
 
   },
