d8ab711092ee03f6c3b9760daf001e61b7d382a0
eveningwater <854806732@qq.com>
2023-10-09 10:31:39 +0800

    feat: 更新了配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,69 @@
 import { defineConfig } from 'dumi';
 
+const footerList = [
+  {
+    text: "eveningwater(夕水)",
+    url: "https://www.eveningwater.com/"
+  },
+  {
+    text: "联系我",
+    url: "mailto:854806732@qq.com"
+  },
+  {
+    text: "github",
+    url: "https://github.com/eveningwater"
+  },
+  {
+    text: "码云",
+    url: "https://gitee.com/eveningwater"
+  },
+  {
+    text: "cn博客",
+    url: "https://www.cnblogs.com/eveningwater/"
+  },
+  {
+    text: "个人博客",
+    url: "https://eveningwater.github.io/"
+  },
+  {
+    text: "思否",
+    url: "https://segmentfault.com/u/xishui_5ac9a340a5484"
+  },
+  {
+    text: "掘金",
+    url: "https://juejin.im/user/5bcfd79de51d45473245dc1c"
+  },
+  {
+    text: "知乎",
+    url: "https://www.zhihu.com/people/eveningwater"
+  },
+  {
+    text: "蜀ICP备17023218号",
+    url: "http://beian.miit.gov.cn/"
+  }
+]
+
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/my-poem-website/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/my-poem-website/' : '/',
   themeConfig: {
-    name: 'poem'
+    name: 'poem',
+    socialLinks: {
+      github: 'https://github.com/eveningwater',
+      zhihu: 'https://www.zhihu.com/people/eveningwater'
+    },
+    rtl: true,
+    styles: [`.pipe-el+.pipe-el:before {
+      content: "";
+      font-size: 0;
+      padding: 12px 3px 1px;
+      margin-left: 6px;
+      border-left: 1px solid #f2f3f4;
+  }`],
+    footer: `<span class="pipe-el">Copyright ©2023</span>
+    ${footerList.map(item => `<a href="${item.url}" target="_blank" rel="noopener noreferrer" class="pipe-el">${item.text}</a>`).join('')}`,
+    editLink: true,
+    lastUpdated: true
   },
   locales: [
     { id: 'zh-CN', name: '中文' },
